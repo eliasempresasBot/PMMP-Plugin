2d7dd3298892ba6c5924f280bfe09db1b07e0bec
ELIAS EMPRESAS <eliasempresas0@gmail.com>
2025-07-14 14:26:27 -0300

    Update BroadcastTask.php

--- src/BroadcastTask.php
+++ src/BroadcastTask.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ExamplePlugin;
+namespace ELIASEMPRESAS;
 
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
@@ -12,6 +12,6 @@ class BroadcastTask extends Task{
 	public function __construct(private Server $server){ }
 
 	public function onRun() : void{
-		$this->server->broadcastMessage("[ExamplePlugin] I've run on tick " . $this->server->getTick());
+		$this->server->broadcastMessage("[ELIASEMPRESAS] I've run on tick " . $this->server->getTick());
 	}
 }
